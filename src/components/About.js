0c2aec7f9ea0bd6d7a6ae4f14526ee4681fcb9b2
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2021-01-27 10:13:06 -0700

    add about page content

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,20 @@ import "../styles/App.css";
 
 const About = () => {
   return (
-    <div>
-      <h1>About Page</h1>
+    <div className={"container"}>
+      <h1>About Us</h1>
+      <img
+        src="https://cdn.custom-cursor.com/collections/129/cover-pokemon-preview.png"
+        alt="lots of mans"
+      />
+      <p className={"text"}>
+        Are you looking to add some new and rare mans to your collection? We
+        have you covered! We have the finest selection of pokemans in all of the
+        Koonto Region. Our collectors travel the world looking for new and
+        interesting mans, which you could take home today! Take a look at our
+        current selection, let us know if you have any requests and we'll get
+        right on it!
+      </p>
     </div>
   );
 };
